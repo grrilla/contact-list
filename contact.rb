c2b9aa12bc564cba2a36fcfc10af2bd359c58876
grrilla <mike.longauer@gmail.com>
2016-07-26 01:08:33 +0000

    Defining interface in contact_list.rb, adding exception handling to contact.rb

--- contact.rb
+++ contact.rb
@@ -4,13 +4,15 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
+  class ContactNotFoundError < StandardError
+  end
+
   attr_accessor :name, :email
   
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
   def initialize(name, email)
-    # TODO: Assign parameter values to instance variables.
     @name = name
     @email = email
   end
@@ -21,7 +23,6 @@ class Contact
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
       CSV.read('contacts.csv')
     end
 
@@ -29,14 +30,15 @@ class Contact
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
     def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
       highest_id = 0
       Contact.all.each do |contact|
-        highest_id = contact[0] if contact[0] > highest_id
+        if contact[0].to_i > highest_id
+          highest_id = contact[0].to_i
+        end
       end
       new_contact = Contact.new(name,email)
       contact_file = CSV.open('contacts.csv', 'a')
-      contact_file << [highest_id + 1, new_contact.name, new_contact.email]
+      contact_file << [(highest_id + 1).to_s, new_contact.name, new_contact.email]
       new_contact
     end
     
@@ -44,19 +46,18 @@ class Contact
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      raise ArgumentError, "You must enter a valid ID." if id.nil?
       Contact.all.each do |contact|
         return contact if contact[0] == id
       end
-      return nil
-      # raise exception if id is not a number
+      raise ContactNotFoundError, "No contact with ID #{id} found."
     end
     
     # Search for contacts by either name or email.
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      raise ArgumentError, "You must enter a valid search term." if term.nil?
       matches = Array.new
       Contact.all.each do |contact|
         matched_term = false
@@ -65,6 +66,7 @@ class Contact
         end
         matches << contact if matched_term
       end
+      raise ContactNotFoundError, "No record containing expression \"#{term}\" found." if matches.empty?
       matches
     end
 
--- contact_list.rb
+++ contact_list.rb
@@ -2,20 +2,55 @@ require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
-
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
   
-  # capture arg, if any
-  
+  if ARGV[0]
+    case ARGV[0].downcase
 
+      when "list" then
+      	counter = 0
+        Contact.all.each do |contact|
+          print "\n#{contact[0]}: #{contact[1]} (#{contact[2]})"
+          counter += 1
+        end
+        puts "\n---\n#{counter} records found\n\n"
+    
+      when "new" then
+        print "\nEnter the new contact's full name: "
+        name = STDIN.gets.chomp
+        print "Now the email address associated with this contact: "
+        email = STDIN.gets.chomp
+        begin
+          Contact.create(name,email)
+          puts "#{name} was saved successfully.\n\n"
+        rescue => e
+          puts "#{e.class}: #{e.message}\n\n"
+        end
+    
+      when "show" then
+      	begin
+          contact = Contact.find(ARGV[1])
+          puts "\n#{contact[0]}: #{contact[1]} (#{contact[2]})\n\n"
+        rescue => e
+          puts "#{e.class}: #{e.message}\n\n"
+        end
 
-  # Command 1: no arg
+      when "search" then
+        begin
+          matches = Contact.search(ARGV[1])
+          matches.each do |contact|
+            puts "\n#{contact[0]}: #{contact[1]} (#{contact[2]})"
+          end
+          puts ""
+        rescue => e
+          puts "#{e.class}: #{e.message}\n\n"
+        end        	
 
-  # COmmand 2: list
-
-  # Command 3: new
-
-  # Command 4: show
-
-  # Command 5: search
+    end
+  else
+  	puts "\nHere is a list of available commands:"
+  	puts "    #{"new".ljust(8)}- Create a new contact"
+  	puts "    #{"list".ljust(8)}- List all contacts"
+  	puts "    #{"show".ljust(8)}- Show a contact"
+  	puts "    #{"search".ljust(8)}- Search contacts\n\n"
+  end
 end
